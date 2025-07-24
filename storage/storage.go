1e26ce48b3da7b4144194a8ecf684ec4ae504be6
Doyoung Kim <lark.noanswer@gmail.com>
2025-07-25 00:08:05 +0900

    Update storage

--- storage/storage.go
+++ storage/storage.go
@@ -1,11 +1,23 @@
 package storage
 
+// Example put request body from OpenTSDB
+// {
+//     "metric": "sys.cpu.nice",
+//     "timestamp": 1346846400,
+//     "value": 18,
+//     "tags": {
+//        "host": "web01",
+//        "dc": "lga"
+//     }
+// }
+
 type DataPoint struct {
-    Timestamp int64
-    Value     float64
+	Timestamp int64
+	Value     float64
 }
 
 type TimeSeries struct {
-    Metric string
-    Points []DataPoint // kept sorted by timestamp
+	Metric string
+	Tags   map[string]string
+	Points []DataPoint // TODO: sort by timestamp when possible
 }
