08bac052307b249524d060d08955916e169206a1
Doyoung Kim <lark.noanswer@gmail.com>
2025-07-25 23:51:31 +0900

    Add metric handler

--- main.go
+++ main.go
@@ -1,12 +1,42 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
+	"time"
 )
+
+// TODO: Proper validation
+type MetricData struct {
+	Metric    string            `json:"metric"`
+	Timestamp int64             `json:"timestamp"`
+	Value     float64           `json:"value"`
+	Tags      map[string]string `json:"tags"`
+}
+
+func metricHandler(w http.ResponseWriter, r *http.Request) {
+	var data MetricData
+	if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
+		http.Error(w, "Invalid payload: "+err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// TODO: Remove if it turns out to be unnecessary
+	if data.Timestamp > time.Now().Unix() {
+		http.Error(w, "Timestamp cannot be in the future", http.StatusBadRequest)
+		return
+	}
+
+	// TODO: Save metric data
+	log.Printf("got metric: %+v", data)
+
+	w.WriteHeader(http.StatusOK)
+}
 
 func main() {
 	mux := http.NewServeMux()
+	mux.HandleFunc("PUT /", metricHandler)
 
 	server := &http.Server{
 		Addr:    ":8000",
--- storage/storage.go
+++ storage/storage.go
@@ -1,16 +1,5 @@
 package storage
 
-// Example put request body from OpenTSDB
-// {
-//     "metric": "sys.cpu.nice",
-//     "timestamp": 1346846400,
-//     "value": 18,
-//     "tags": {
-//        "host": "web01",
-//        "dc": "lga"
-//     }
-// }
-
 type DataPoint struct {
 	Timestamp int64
 	Value     float64
