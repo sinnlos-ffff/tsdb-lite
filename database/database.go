633d86ea86ed0b94fb2feb692f3e693002b81fe6
Doyoung Kim <lark.noanswer@gmail.com>
2025-07-26 15:48:02 +0900

    Add struct for database

--- database/database.go
+++ database/database.go
@@ -1,12 +1,39 @@
 package database
 
+import "sync"
+
 type Point struct {
 	Timestamp int64
 	Value     float64
 }
 
 type TimeSeries struct {
+	sync.Mutex
 	Metric string
 	Tags   map[string]string
 	Points []Point // TODO: sort by timestamp when possible
+}
+
+type Database struct {
+	sync.RWMutex
+	series map[string]*TimeSeries
+}
+
+func NewDatabase() *Database {
+	return &Database{
+		series: make(map[string]*TimeSeries),
+	}
+}
+
+func (db *Database) AddTimeSeries(metric string, tags map[string]string) {
+	db.Lock()
+	defer db.Unlock()
+
+	ts := TimeSeries{
+		Metric: metric,
+		Tags:   tags,
+		Points: make([]Point, 0),
+	}
+
+	db.series[metric] = &ts
 }
