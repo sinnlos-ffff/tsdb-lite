0a8a9160bdb465f21e896fa1f1360834fefcd724
Doyoung Kim <lark.noanswer@gmail.com>
2025-08-20 01:05:11 +0900

    Initialize loop function for WAL

--- database/wal.go
+++ database/wal.go
@@ -17,3 +17,28 @@ type WAL struct {
 	f         *os.File
 	flushTick *time.Ticker // e.g., 20ms
 }
+
+func (w *WAL) loop() {
+	buf := make([]byte, 0, 1<<20)
+	for {
+		select {
+		case req := <-w.ch:
+			// TODO: Implement encoding logic
+			buf = appendRecord(buf, req.p)
+			if len(buf) > 64<<10 { // 64KiB batch
+				w.w.Write(buf)
+				buf = buf[:0]
+			}
+
+			// TODO: Consider moving ACK after w.f.Sync()
+			req.done <- nil
+		case <-w.flushTick.C:
+			if len(buf) > 0 {
+				w.w.Write(buf)
+				buf = buf[:0]
+			}
+			w.w.Flush()
+			w.f.Sync() // group commit
+		}
+	}
+}
